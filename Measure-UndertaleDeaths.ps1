fc80aa3f505b55095b0a5819d552a627221b1ff9
Mark Kraus <github@markekraus.com>
2019-12-15 02:38:41 -0800

    Initial commit

---
+++ Measure-UndertaleDeaths.ps1
@@ -0,0 +1,53 @@
+<#
+    .SYNOPSIS
+        Updates a file with the total deaths in Undertale.
+    .DESCRIPTION
+        On Windows system, this script will parse the undertale.ini and update a file with the
+        total death count. This can be used by OBS as a text source to display the current 
+        death count you have taken in Undertale.
+    .PARAMETER IniFile
+        The full paul to the undertale.ini file. defaults to %LOCALAPPDATA%\UNDERTALE\undertale.ini
+    .PARAMETER OutFile
+        The output file that the death count in which the death count will be stored. This
+        file can be used as a text source in OBS. Defaults to 
+        %LOCALAPPDATA%\UndertaleDeathCount.txt
+    .PARAMETER Message
+        The message that will be set in the OutFile. Use '{0}' to represent the death count.
+        Defaults to 'Undertale Deaths: {0}'.
+    .EXAMPLE
+        & ./Measure-UndertaleDeaths.ps1
+    .EXAMPLE
+        & ./Measure-UndertaleDeaths.ps1 -Message 'Deaths: {0}'
+    .EXAMPLE
+        & ./Measure-UndertaleDeaths.ps1 -OutFile c:\DeathCount.txt
+    .EXAMPLE
+        & ./Measure-UndertaleDeaths.ps1 -IniFile 'C:\Users\Mark\AppData\Local\UNDERTALE\undertale.ini'
+    .NOTES
+        This currently only supports Undertale on Windows.
+#>
+[CmdletBinding()]
+param(
+    [Parameter()]
+    [String]
+    $IniFile = (Join-Path $env:LOCALAPPDATA "UNDERTALE\undertale.ini"),
+
+    [Parameter()]
+    [String]
+    $OutFile = (Join-Path $env:LOCALAPPDATA "UndertaleDeathCount.txt"),
+
+    [Parameter()]
+    [String]
+    $Message = 'Undertale Deaths: {0}'
+)
+
+while ($true) {
+    try {
+        $Result = Get-Content $IniFile -ErrorAction Stop | Select-String "gameover"
+        [int]$Count = ($Result.Line -split '=')[1] -replace '"'
+        $Content = $Message -f $Count
+        Clear-Host
+        Write-Host $Content
+        $Content | Set-Content $OutFile -ErrorAction Stop
+        Start-Sleep -Milliseconds 500
+    } catch {}
+}
---
+++ README.md
@@ -0,0 +1,24 @@
+# Measure Undertale Deaths
+
+This script will create a file that can be used in OBS to display the current total death count.
+
+# Usage
+
+In a PowerShell console, run the following from the folder where you have stored the script.
+
+```powershell
+& ./Measure-UndertaleDeathes.ps1
+```
+
+In OBS add a Text source for the file `%LOCALAPPDATA%\UndertaleDeathCount.txt`.
+The file path can be changed by specifying `OutFile`.
+
+```powershell
+& ./Measure-UndertaleDeathes.ps1 -OutFile 'c:\DeathCount.txt'
+```
+
+You can also customize the text using the `-Message` Parameter.
+
+```powershell
+& ./Measure-UndertaleDeathes.ps1 -OutFile 'c:\DeathCount.txt' -Message '{0} Deaths'
+```
